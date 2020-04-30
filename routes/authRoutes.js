9a2db8c32a92ea6ce8387605d50b4faefaa38635
Ritesh Patil <riteshsp2000@gmail.com>
2020-05-01 03:15:43 +0530

    Changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,13 @@
 const passport = require('passport');
 
 module.exports = (app) => {
+  
+  app.get('/', (req, res) => {
+    res.send({
+      Hi: 'hi'
+    })
+  })
+
   app.get(
     '/auth/google', 
     passport.authenticate('google', {
