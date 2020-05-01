b89928fdb091c17ab54a90872d38b3fbe7f2ec2a
Ritesh Patil <riteshsp2000@gmail.com>
2020-05-01 12:03:19 +0530

    another commit

--- services/passport.js
+++ services/passport.js
@@ -26,7 +26,8 @@ passport.use(
     {
       clientID: keys.googleClientID,                    // These represent the developers google oauth console details.
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     }, 
     (accessToken, refreshToken, profile, done) => {
       User.findOne({googleId: profile.id})
