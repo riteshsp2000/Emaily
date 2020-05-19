f2430bddb3083dadab1edd3550ab7bbc30dc9a6b
Ritesh Patil <riteshsp2000@gmail.com>
2020-05-20 02:09:29 +0530

    Updated the Dashboard to show the list of surveys

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
-import createBrowserHistory from 'react-router-dom';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 export const fetchUser = () => async (dispatch) => {
   const res = await axios.get('/api/current_user');
@@ -19,4 +18,10 @@ export const submitSurvey = (values, history) => async (dispatch) => {
 
   dispatch({ type: FETCH_USER, payload: res.data });
   history.push('/surveys');
+};
+
+export const fetchSurveys = () => async (dispatch) => {
+  const res = await axios.get('/api/surveys');
+
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
---
+++ client/src/components/surveys/SurveyList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchSurveys } from '../../actions';
+
+class SurveyList extends React.Component {
+  componentDidMount() {
+    this.props.fetchSurveys();
+  }
+
+  renderSurveys() {
+    return this.props.surveys.reverse().map((survey) => {
+      return (
+        <div className='card green darken-1' key={survey._id}>
+          <div className='card-content'>
+            <span className='card-title'>{survey.title}</span>
+            <p>{survey.body}</p>
+            <p className='right'>
+              Sent on: {new Date(survey.dateSent).toLocaleString()}
+            </p>
+            <div className='card-action'>
+              <a>Yes: {survey.yes}</a>
+              <a>No: {survey.no}</a>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return <div>{this.renderSurveys()}</div>;
+  }
+}
+
+function mapStateToProps({ surveys }) {
+  return { surveys };
+}
+
+export default connect(mapStateToProps, { fetchSurveys })(SurveyList);
